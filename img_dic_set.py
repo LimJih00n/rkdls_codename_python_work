aa042428b67ca32857249eddcb3d8ba1e9b964a4
ssggiisgi <gishin0410@gmail.com>
2024-03-19 13:23:39 +0900

    dev5

---
+++ img_dic_set.py
@@ -0,0 +1,132 @@
+
+# warrior: 1152-6: 192-100
+#tree 768-4 -180
+#sheep 768-6 - 60
+#gold 896-7 - 80
+#goblin 1152-6-100
+knight_img_dic ={
+    "default":
+        {
+            "frame":6,
+            "f-width-full":1152,
+            "f-width": 100,
+            "f-height":100,
+            "img-url": './assets/knight/right/w-right.png'
+        },
+    "left":{
+            "frame":6,
+            "f-width-full":1152,
+            "f-width": 100,
+            "f-height":100,
+            "img-url": './assets/knight/left/w-left.png'
+        },
+    "right":{
+            "frame":6,
+            "f-width-full":1152,
+            "f-width": 100,
+            "f-height":100,
+            "img-url": './assets/knight/right/w-right.png'
+        }
+}
+sheep_img_dic = {
+    "default":{
+            "frame":6,
+            "f-width-full":768,
+            "f-width":100,
+            "f-height":60,
+            "img-url": './assets/objects/sheep/right/sheep-right.png'
+        },
+    "right":{
+            "frame":6,
+            "f-width-full":768,
+            "f-width":100,
+            "f-height":60,
+            "img-url": './assets/objects/sheep/right/sheep-right.png'
+        },
+    "left":{
+            "frame":6,
+            "f-width-full":768,
+            "f-width":100,
+            "f-height":60,
+            "img-url": './assets/objects/sheep/left/sheep-left.png'
+        },
+}
+
+tree_img_dic = {
+    "default":{
+            "frame":4,
+            "f-width-full":768,
+            "f-width": 192,
+            "f-height":180,
+            "img-url": './assets/objects/tree/Tree.png'
+    }
+}
+goblin_img_dic = {
+     "default":
+        {
+            "frame":6,
+            "f-width-full":1152,
+            "f-width": 192,
+            "f-height":100,
+            "img-url": './assets/goblin/right/g-right.png'
+        },
+    "left":{
+            "frame":6,
+            "f-width-full":1152,
+            "f-width": 192,
+            "f-height":100,
+            "img-url": './assets/goblin/left/g-left.png'
+        },
+    "right":{
+            "frame":6,
+            "f-width-full":1152,
+            "f-width": 192,
+            "f-height":100,
+            "img-url": './assets/goblin/right/g-right.png'
+        }
+}
+Castle_img_dic = {
+    "default":{
+        "frame":1,
+        "f-width-full":195,
+        "f-width":195,
+        "f-height":288,
+        "img-url":'./assets/Buildings/Castle/Castle_Blue.png'
+    }
+}
+House_img_dic = {
+      "default":{
+        "frame":1,
+        "f-width-full":108,
+        "f-width":108,
+        "f-height":142,
+        "img-url":'./assets/Buildings/House/House_Blue.png'
+    }
+}
+Tower_img_dic ={
+     "default":{
+        "frame":1,
+        "f-width-full":115,
+        "f-width":115,
+        "f-height":173,
+        "img-url":'./assets/Buildings/Tower/Tower_Blue.png'
+    }
+}
+background_img_dic={
+     "default":{
+        "frame":1,
+        "f-width-full":3300,
+        "f-width":3300,
+        "f-height":3300,
+        "img-url":'./assets/background/grid_tile.png'
+    }
+}
+gold_img_dic={
+    "default":{
+        "frame":7,
+        "f-width-full":896,
+        "f-width":100,
+        "f-height":80,
+        "img-url":'./assets/objects/gold/G_Spawn.png'
+    }
+}
